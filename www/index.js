7dd89601a727f9153509be941e80b0233b714ada
Tim Banon <3849997+thebanon@users.noreply.github.com>
2021-03-06 06:18:43 -0500

    Import repository

--- www/index.js
+++
@@ -1,9 +0,0 @@
-document.domain='uios.me';
-function byId(id) { return document.getElementById(id); }
-function webCam(video) {
-  navigator.mediaDevices.getUserMedia({audio: true, video: { facingMode: "user" }}).then(stream => {
-    video.srcObject = null;
-    video.srcObject = window.localStream = stream;
-    video.onloadedmetadata = function(e) { e.srcElement.play(); }
-  });
-}
